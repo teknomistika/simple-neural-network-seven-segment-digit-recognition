6a0eb0ee318a20d15b8227689c6611251f0c427d
Mochamad Arifin <klampok.child@gmail.com>
2026-02-22 15:07:51 +0700

    style box training

--- src/components/TrainingStage.vue
+++ src/components/TrainingStage.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="d-flex" :style="{ aspectRatio: `${ratio.w} / ${ratio.h}` }">
+    <div class="stage" :style="{ aspectRatio: `${ratio.w} / ${ratio.h}` }">
         <svg ref="svgEl" width="100%" height="100%" :viewBox="`0 0 ${dim.x} ${dim.y}`"
             xmlns="http://www.w3.org/2000/svg">
             <!-- <rect width="100%" height="100%" fill="white" stroke="none" /> -->
@@ -7,19 +7,66 @@
                 <line v-for="(p, i) in lines" x1="0" v-bind="p" :key="`${k}${i}`" stroke="#888" stroke-width=".1" />
             </template>
 
-            <!-- Number to animate -->
-            <text x="10" y="10" id="movingText" font-size="10" fill="orange">5</text>
-        </svg>
-        <!-- <v-row>
-            <v-col>
-                <v-sheet>
+            <!-- <rect x="10" y="10" width="10" height="10" fill="#2563eb" Xstroke="#1d4ed8" Xstroke-width="2" /> -->
 
-                </v-sheet>
-            </v-col>
-        </v-row> -->
+            <!-- Number to animate -->
+            <!-- <text x="15" y="15" id="movingText" font-size="10" fill="orange" text-anchor="middle"
+                dominant-baseline="middle">5</text> -->
+        </svg>
+        <div class="forward border">
+            <div class="box inputs">
+                <p>Input (x)</p>
+                <div class="x">
+                    1
+                </div>
+                <div class="x">
+                    2
+                </div>
+                <div class="x">
+                    3
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+<style>
+.stage {
+    display: block;
+    position: relative;
 
+    >svg {
+        position: absolute;
+    }
+
+    .forward {
+        /* height: 30%; */
+        display: flex;
+        background-color: rgba(0, 166, 255, 0.327);
+    }
+}
+
+
+.stage .box {
+    text-align: center;
+    gap: 2px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+}
+
+.stage .box.inputs {
+    margin-left: 5%;
+    width: 5%;
+    >.x {
+        border: solid 1px red;
+        background-color: red;
+        aspect-ratio: 1;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+    }
+}
+</style>
 <script setup lang="ts">
 
 import { SVG } from '@svgdotjs/svg.js'
@@ -76,8 +123,3 @@ onMounted(() => {
 onUnmounted(() => svg?.remove())
 
 </script>
-<style>
-/* .stage {
-    aspect-ratio: 1.7777777777777777;
-} */
-</style>
